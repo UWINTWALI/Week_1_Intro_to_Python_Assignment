b0938eafbadbb2621e257c758a4923e3a91e44d3
JEAN DE DIEU UWINTWALI <uwintwalijeandedieu3@gmail.com>
2024-08-25 20:18:16 +0200

    Python program to take user input and use it to display a personalized message

---
+++ user_input.py
@@ -0,0 +1,5 @@
+
+name = input("What is Your Name: ")
+age = input("How Old Are You: ")
+location = input("Where Do You Live: ")
+print(f"Hello {name}, you are {age} years old and live in {location}.")
